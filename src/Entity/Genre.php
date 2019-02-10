ef844a4505177d8d0f1728e97b58eba6de0fa2e9
roboloop <12248453+roboloop@users.noreply.github.com>
2019-02-10 16:25:16 +0000

    Add description field to Genre entity

--- src/Entity/Genre.php
+++ src/Entity/Genre.php
@@ -39,6 +39,11 @@ class Genre
      */
     private $topics;
 
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
     public function __construct()
     {
         $this->topics = new ArrayCollection();
@@ -97,6 +102,18 @@ class Genre
             $this->topics->removeElement($topic);
             $topic->removeGenre($this);
         }
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
 
         return $this;
     }
