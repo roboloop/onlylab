38a6c80b9949793b0c0383ca956b346166332a5d
roboloop <12248453+roboloop@users.noreply.github.com>
2019-02-10 03:59:13 +0000

    Add TopicAssembler and TitleParserService to construct Topic entities

---
+++ src/Assembler/TopicAssembler.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Assembler;
+
+use App\Bag\Bag;
+use App\Entity\Topic;
+use App\Resolver\DateResolver;
+use App\Resolver\SizeResolver;
+use App\Service\TitleParserService;
+use App\Validator\TopicBagValidator;
+
+class TopicAssembler
+{
+    /** @var \App\Validator\TopicBagValidator */
+    private $bagValidator;
+
+    /** @var \App\Service\TitleParserService */
+    private $titleParser;
+
+    /** @var \App\Resolver\SizeResolver */
+    private $sizeResolver;
+
+    /** @var \App\Resolver\DateResolver */
+    private $dateResolver;
+
+    public function __construct(
+        TopicBagValidator $bagValidator,
+        TitleParserService $titleParser,
+        SizeResolver $sizeResolver,
+        DateResolver $dateResolver
+    )
+    {
+        $this->bagValidator = $bagValidator;
+        $this->titleParser  = $titleParser;
+        $this->sizeResolver = $sizeResolver;
+        $this->dateResolver = $dateResolver;
+    }
+
+    /**
+     * @param \App\Bag\Bag $bag
+     *
+     * @return \App\Entity\Topic
+     * @throws \Exception
+     */
+    public function make(Bag $bag)
+    {
+        $this->bagValidator->validate($bag);
+        $quality        = $this->titleParser->getQuality($bag['title']);
+        $year           = $this->titleParser->getYear($bag['title']);
+        $studio         = $this->titleParser->getStudio($bag['title']);
+        $size           = $this->sizeResolver->resolve($bag['size']);
+        $trackerCreatedAt   = $this->dateResolver->resolve($bag['trackerCreatedAt']);
+        $releaseAt      = $this->dateResolver->resolve($bag['releaseAt']);
+
+        $topic = new Topic();
+        return $topic
+            ->setTitle($bag['title'])
+            ->setStudio($studio)
+            ->setYear($year)
+            ->setQuality($quality)
+            ->setSize($size)
+            ->setTrackerCreatedAt($trackerCreatedAt)
+            ->setTrackerId($bag['trackerId'])
+            ->setReleaseAt($releaseAt)
+            ;
+
+    }
+}
---
+++ src/Service/TitleParserService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Service;
+
+use App\Loader\ParsersLoader;
+use App\Parser\Title\QualityParser;
+use App\Parser\Title\StudioParser;
+use App\Parser\Title\YearParser;
+
+class TitleParserService
+{
+    /** @var \App\Loader\ParsersLoader */
+    private $loader;
+
+    public function __construct(ParsersLoader $loader)
+    {
+        $this->loader = $loader;
+    }
+
+    public function getQuality(string $title)
+    {
+        return $this->loader->getParser(QualityParser::class)->parse($title);
+    }
+
+    public function getYear(string $title)
+    {
+        return $this->loader->getParser(YearParser::class)->parse($title);
+    }
+
+    public function getStudio(string $title)
+    {
+        return $this->loader->getParser(StudioParser::class)->parse($title);
+    }
+}
