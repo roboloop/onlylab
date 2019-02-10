602390dd83afb1a13ac194cde8167409d7850aa0
roboloop <12248453+roboloop@users.noreply.github.com>
2019-02-10 16:24:48 +0000

    Add database backup and restore via BackupManager with MySQL dump/restore tweaks
    - Expose backup/restore operations through DatabaseManager
    - Adjust mysqldump to exclude schema/triggers; disable FK checks on restore

--- src/System/MysqlDatabase.php
+++ src/System/MysqlDatabase.php
@@ -2,7 +2,28 @@
 
 namespace App\System;
 
-class MysqlDatabase
-{
+use BackupManager\Databases\MysqlDatabase as BaseMysqlDatabase;
 
+class MysqlDatabase extends BaseMysqlDatabase
+{
+    public function getDumpCommandLine($outputPath)
+    {
+        $path = parent::getDumpCommandLine($outputPath);
+
+        $path = preg_replace('/--routines/', '', $path);
+
+        $path = preg_replace('/>/', '--no-create-info --skip-triggers --no-create-db --no-tablespaces --compact >', $path);
+
+        return $path;
+    }
+
+    public function getRestoreCommandLine($inputPath)
+    {
+        // These settings are located here because the symfony bundle did not provide extraParams as a parameter in the config.
+        $path = parent::getRestoreCommandLine($inputPath);
+
+        $path = $path . ' --init-command "SET FOREIGN_KEY_CHECKS=0;"';
+
+        return $path;
+    }
 }
