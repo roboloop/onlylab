a85034716b75742d9e93d24cb49fb962cf378e5a
roboloop <12248453+roboloop@users.noreply.github.com>
2019-03-17 17:53:15 +0000

    Add ArrayTransformer to rekey arrays by property and lowercase values

---
+++ src/Service/Transformer/ArrayTransformer.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Service\Transformer;
+
+use Symfony\Component\PropertyAccess\PropertyAccess;
+
+class ArrayTransformer
+{
+    public function setKeyFromSource(array $genres, string $keyName, bool $lowerKey = false)
+    {
+        foreach ($genres as $genre) {
+            $key = PropertyAccess::createPropertyAccessor()->getValue($genre, $keyName);
+
+            if ($lowerKey) {
+                $key = mb_strtolower($key);
+            }
+
+            $result[$key] = $genre;
+        }
+
+        return $result ?? [];
+    }
+
+    public function toLower(array $data)
+    {
+        array_walk($data, function (&$value) {
+            $value = mb_strtolower($value);
+        });
+
+        return $data;
+    }
+}
