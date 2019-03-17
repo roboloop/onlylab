7ed14e29a0fd4a6e100911c262c641255aef7274
roboloop <12248453+roboloop@users.noreply.github.com>
2019-03-17 18:00:26 +0000

    Remove unused GenreStatus import

--- src/Entity/Genre.php
+++ src/Entity/Genre.php
@@ -3,7 +3,6 @@
 namespace App\Entity;
 
 use App\Entity\Traits\TimestampableEntity;
-use App\Constant\GenreStatus;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
