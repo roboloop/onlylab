a17fb1bffacea17c6b6b01ab7b1a396c1906f146
roboloop <12248453+roboloop@users.noreply.github.com>
2019-03-18 00:37:22 +0000

    Add Router service skeleton

---
+++ src/Service/Router/Router.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Service\Router;
+
+class Router
+{
+    public function topic($id)
+    {
+        // TODO:
+    }
+
+    public function forum($id, $page = 1)
+    {
+        // TODO:
+    }
+
+    public function torrent($id)
+    {
+        // TODO:
+    }
+}
