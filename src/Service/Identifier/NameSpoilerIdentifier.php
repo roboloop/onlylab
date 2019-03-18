54f1b6596a46c04b5bc3897fe24e08012a097512
roboloop <12248453+roboloop@users.noreply.github.com>
2019-03-18 00:30:19 +0000

    Add NameSpoilerIdentifier to detect screenshots, screenlists and GIFs

--- src/Service/Identifier/NameSpoilerIdentifier.php
+++ src/Service/Identifier/NameSpoilerIdentifier.php
@@ -4,5 +4,45 @@ namespace App\Service\Identifier;
 
 class NameSpoilerIdentifier
 {
+    private $screenshotNames = [
+        'Скриншот',
+        'Скриншоты',
+        'Screenshots',
+        'Примеры',
+    ];
 
+    private $screenListingNames = [
+        'Скринлист',
+        'ScreenListing',
+    ];
+
+    private $gifNames = [
+        'GIF',
+    ];
+
+    public function isScreenshots(string $header)
+    {
+        return $this->search($header, $this->screenshotNames);
+    }
+
+    public function isScreenListing(string $header)
+    {
+        return $this->search($header, $this->screenListingNames);
+    }
+
+    public function isGif(string $header)
+    {
+        return $this->search($header, $this->gifNames);
+    }
+
+    private function search(string $header, array $names)
+    {
+        foreach ($names as $name) {
+            if (false !== mb_eregi($name, $header)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
