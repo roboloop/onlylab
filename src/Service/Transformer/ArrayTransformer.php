920e1cbdaece1639ab3fb65b11d965bee4b2d558
roboloop <12248453+roboloop@users.noreply.github.com>
2019-04-20 22:59:13 +0000

    Reuse PropertyAccessor in ArrayTransformer to reduce overhead

--- src/Service/Transformer/ArrayTransformer.php
+++ src/Service/Transformer/ArrayTransformer.php
@@ -8,8 +8,10 @@ class ArrayTransformer
 {
     public function setKeyFromSource(array $genres, string $keyName, bool $lowerKey = true)
     {
+        $propertyAccessor = PropertyAccess::createPropertyAccessor();
+
         foreach ($genres as $genre) {
-            $key = PropertyAccess::createPropertyAccessor()->getValue($genre, $keyName);
+            $key = $propertyAccessor->getValue($genre, $keyName);
 
             if ($lowerKey) {
                 $key = mb_strtolower($key);
