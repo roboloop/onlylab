e504f5b79c14b2ad42c77ab3ae8d496e724a175c
roboloop <12248453+roboloop@users.noreply.github.com>
2019-05-11 17:52:04 +0000

    Add endpoint to list topics for a genre

--- src/Controller/GenreController.php
+++ src/Controller/GenreController.php
@@ -2,23 +2,58 @@
 
 namespace App\Controller;
 
+use App\Entity\Genre;
 use App\Service\GenreService;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-class GenreController extends AbstractController
+class GenreController extends BaseController
 {
     /**
-     * @Route("/genres", methods={"GET"})
+     * @Route("/genres", methods={"GET"}, name="genres")
      * @param \App\Service\GenreService $genreService
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function genresWithTopics(GenreService $genreService)
     {
-        $genres = $genreService->genresWithTopics();
-        // $genres = $genreService->findAll();
+        $genres = $genreService->findAll();
 
         return $this->render('genre/index.html.twig', [
             'genres' => $genres
+        ]);
+    }
+
+    /**
+     * Get topics of this genre
+     *
+     * @Route("/genres/{genre}/topics", methods={"GET"}, name="genres_topics")
+     * @ParamConverter("genre", options={
+     *      "mapping": {"genre":"id"}
+     * })
+     *
+     * @param \App\Entity\Genre                         $genre
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param \Knp\Component\Pager\PaginatorInterface   $paginator
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function topics(Genre $genre, Request $request, PaginatorInterface $paginator)
+    {
+        // for ($i = 0; $i < 180; $i++) {
+        //     $arr[] = rand(1,20);
+        // }
+        //
+        // $result = $paginator->paginate($arr ?? [], 2, 25, [ 'distinct' => false]);
+
+        $topics = $genre->getTopics();
+
+
+        return $this->render('genre/topics.html.twig', [
+            'genre'     => $genre,
+            'topics'    => $topics,
         ]);
     }
 }
