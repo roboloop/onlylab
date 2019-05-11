2309c2eebe637c39b8bcd646592ded75172970a1
roboloop <12248453+roboloop@users.noreply.github.com>
2019-05-11 17:17:28 +0000

    Add optional name argument to set dump filename in db:dump:create

--- src/Command/CreateDumpCommand.php
+++ src/Command/CreateDumpCommand.php
@@ -4,6 +4,7 @@ namespace App\Command;
 
 use App\System\DatabaseManager;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -24,6 +25,7 @@ class CreateDumpCommand extends Command
         $this
             ->setName('db:dump:create')
             ->setDescription('Create a dump of database.')
+            ->addArgument('name', InputArgument::OPTIONAL, 'Filename of dump')
             ->addOption('null', null, InputOption::VALUE_NONE, 'Null driver');
     }
 
@@ -31,7 +33,14 @@ class CreateDumpCommand extends Command
     {
         $driver = $input->getOption('null') ? 'null' : 'gzip';
 
-        $filename = (new \DateTime())->format('Y-m-d_H-i-s') . '.sql';
+        $filename = $input->getArgument('name');
+
+        if (null === $filename) {
+            $filename = (new \DateTime())->format('Y-m-d_H-i-s');
+        }
+
+        $filename .= '.sql';
+
         $this->databaseBackup->backup($filename, $driver);
 
          $output->writeln([
