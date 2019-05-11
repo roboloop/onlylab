82590accb0dff1a5aa5f875eaf2d743d65bcf17f
roboloop <12248453+roboloop@users.noreply.github.com>
2019-05-11 17:11:54 +0000

    Add mb_strcasecmp polyfill and register Composer files autoload

---
+++ helpers/helpers.php
@@ -0,0 +1,11 @@
+<?php
+
+if (!function_exists('mb_strcasecmp')) {
+    function mb_strcasecmp($str1, $str2, $encoding = null)
+    {
+        if (null === $encoding) {
+            $encoding = mb_internal_encoding();
+        }
+        return strcmp(mb_strtoupper($str1, $encoding), mb_strtoupper($str2, $encoding));
+    }
+}
