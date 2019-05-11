e7ec9749ed7f55c9cafb217ed3a7fcc1d612c267
roboloop <12248453+roboloop@users.noreply.github.com>
2019-05-11 17:48:02 +0000

    Render root route via Twig template and add route name

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,49 +2,17 @@
 
 namespace App\Controller;
 
-use App\Client\TrackerClient;
-use App\Entity\Genre;
-use App\Repository\GenreRepository;
-use App\Service\Collection\GenreCollection;
-use App\Service\Handler\ForumPageHandler;
-use App\Service\Handler\TopicPageHandler;
-use App\Service\Script\CombForum;
-use App\Service\Script\CombTopic;
-use App\Service\UrlConverter\ImageUrlConverter;
-use Doctrine\Common\Collections\Criteria;
-use Doctrine\ORM\Query\Expr\Andx;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends BaseController
 {
     /**
-     * @Route("/", methods={"GET"})
+     * @Route("/", methods={"GET"}, name="page_main")
      *
-     * @param \App\Client\TrackerClient                 $client
-     * @param \App\Service\Handler\ForumPageHandler $forumPageWorker
-     * @param \App\Service\Handler\TopicPageHandler $topicPageWorker
-     *
-     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function page(CombForum $combForum, CombTopic $combTopic)
+    public function page()
     {
-        $combForum->execute(2007);
-        $combTopic->execute(2505688);
-
-        // $response = $client->get('/forum/viewforum.php?f=7133');
-        // $content = $response->getBody()->getContents();
-
-        // $content = file_get_contents('../examples/forum_page.html');
-        // $content = file_get_contents('../examples/topic_page.html');
-        // $content = file_get_contents('../examples/topic_page.html');
-        // $content = file_get_contents('../examples/dum.html');
-
-        // $result = $forumPageWorker->pageIs($content);
-        // $result = $forumPageWorker->handleAuth($content);
-
-        // $this->transaction($result);
-        // $result = $topicPageWorker->handleAuth($content);
-
-        return $this->json(['Good']);
+        return $this->render('page/main.html.twig');
     }
 }
