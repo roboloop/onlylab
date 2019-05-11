f736f44acb078c130f03403d3f20ca623d77221e
roboloop <12248453+roboloop@users.noreply.github.com>
2019-05-11 17:32:40 +0000

    Add Doctrine EntityManager accessor to BaseController

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Behavior\TransactionManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BaseController extends AbstractController
@@ -16,10 +17,20 @@ class BaseController extends AbstractController
         $this->container->get('app.util.transaction_manager')->transaction(...func_get_args());
     }
 
+    protected function getEm(): EntityManagerInterface
+    {
+        if (!$this->has('doctrine.orm.entity_manager')) {
+            throw new \LogicException('No default entity manager');
+        }
+
+        return $this->container->get('doctrine.orm.entity_manager');
+    }
+
     public static function getSubscribedServices()
     {
         return array_merge(parent::getSubscribedServices(), [
             'app.util.transaction_manager'  => '?'.TransactionManager::class,
+            'doctrine.orm.entity_manager'   => '?'.EntityManagerInterface::class,
         ]);
     }
 }
