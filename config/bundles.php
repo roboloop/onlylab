daa372b7c1954611a67a336241383fe7a8ed484e
roboloop <12248453+roboloop@users.noreply.github.com>
2019-05-11 17:14:59 +0000

    Add KnpPaginatorBundle and default pagination config

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     BM\BackupManagerBundle\BMBackupManagerBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Snc\RedisBundle\SncRedisBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
