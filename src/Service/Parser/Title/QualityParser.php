980391ee70590f95b421f07ee0baad5df47a4896
roboloop <12248453+roboloop@users.noreply.github.com>
2019-05-19 02:20:22 +0000

    Expand title quality parser to detect 4K and UltraHD

--- src/Service/Parser/Title/QualityParser.php
+++ src/Service/Parser/Title/QualityParser.php
@@ -8,7 +8,7 @@ class QualityParser implements ParserInterface
 {
     public function parse(string $string)
     {
-        preg_match('~(\d{3,4}p)~i', $string, $result);
+        preg_match('~(\d{3,4}p|4k|UltraHD)~i', $string, $result);
 
         return $result[1] ?? null;
     }
