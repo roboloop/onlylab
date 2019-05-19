d16a1caac07b02b2d0d80d3bfe339fd3562005c3
roboloop <12248453+roboloop@users.noreply.github.com>
2019-05-19 04:02:25 +0000

    Add approve/reject/ban/unban endpoints for studios

--- src/Controller/StudioController.php
+++ src/Controller/StudioController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Constant\StudioStatus;
 use App\Entity\Genre;
 use App\Entity\Studio;
 use App\Service\GenreService;
@@ -34,5 +35,89 @@ class StudioController extends BaseController
             'studio'    => $studio,
             'topics'    => $topics,
         ]);
+    }
+
+    /**
+     * Approve the studio
+     *
+     * @Route("/studios/{studio}/approve", methods={"POST"}, name="studios_approve")
+     * @ParamConverter("studio", options={
+     *      "mapping": {"studio":"id"}
+     * })
+     *
+     * @param \App\Entity\Studio $studio
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function approve(Studio $studio)
+    {
+        $studio->setIsApproved(true);
+
+        $this->transaction($studio);
+
+        return $this->redirectToRoute('studios_topics', ['studio' => $studio->getId()]);
+    }
+
+    /**
+     * Reject the studio
+     *
+     * @Route("/studios/{studio}/reject", methods={"POST"}, name="studios_reject")
+     * @ParamConverter("studio", options={
+     *      "mapping": {"studio":"id"}
+     * })
+     *
+     * @param \App\Entity\Studio $studio
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function reject(Studio $studio)
+    {
+        $studio->setIsApproved(false);
+
+        $this->transaction($studio);
+
+        return $this->redirectToRoute('studios_topics', ['studio' => $studio->getId()]);
+    }
+
+    /**
+     * Ban the studio
+     *
+     * @Route("/studios/{studio}/ban", methods={"POST"}, name="studios_ban")
+     * @ParamConverter("studio", options={
+     *      "mapping": {"studio":"id"}
+     * })
+     *
+     * @param \App\Entity\Studio $studio
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function ban(Studio $studio)
+    {
+        $studio->setStatus(StudioStatus::BANNED);
+
+        $this->transaction($studio);
+
+        return $this->redirectToRoute('studios_topics', ['studio' => $studio->getId()]);
+    }
+
+    /**
+     * Unban the studio
+     *
+     * @Route("/studios/{studio}/unban", methods={"POST"}, name="studios_unban")
+     * @ParamConverter("studio", options={
+     *      "mapping": {"studio":"id"}
+     * })
+     *
+     * @param \App\Entity\Studio $studio
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function unban(Studio $studio)
+    {
+        $studio->setStatus(StudioStatus::TYPICAL);
+
+        $this->transaction($studio);
+
+        return $this->redirectToRoute('studios_topics', ['studio' => $studio->getId()]);
     }
 }
