b9574cf1af5108e147eae1ef5a941cbf93d8f840
roboloop <12248453+roboloop@users.noreply.github.com>
2019-05-19 04:06:00 +0000

    Add ImageController stub

---
+++ src/Controller/ImageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use App\Constant\StudioStatus;
+use App\Entity\Genre;
+use App\Entity\Studio;
+use App\Service\GenreService;
+use Knp\Component\Pager\PaginatorInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ImageController extends BaseController
+{
+    //
+}
