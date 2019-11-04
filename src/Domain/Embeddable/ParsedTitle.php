79bacd0f72c869ec58d91af8d20c23e54055644d
roboloop <12248453+roboloop@users.noreply.github.com>
2019-11-04 18:09:47 +0000

    Add ParsedTitle embeddable value object

---
+++ src/Domain/Embeddable/ParsedTitle.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Domain\Embeddable;
+
+class ParsedTitle
+{
+    private $rawTitle;
+    private $title;
+    private $year;
+    private $quality;
+
+    public function __construct(string $rawTitle, ?string $title, ?string $year, ?string $quality)
+    {
+        $this->rawTitle = $rawTitle;
+        $this->title    = $title;
+        $this->year     = $year;
+        $this->quality  = $quality;
+    }
+
+    public function getRawTitle(): string
+    {
+        return $this->rawTitle;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function getYear(): ?string
+    {
+        return $this->year;
+    }
+
+    public function getQuality(): ?string
+    {
+        return $this->quality;
+    }
+}
