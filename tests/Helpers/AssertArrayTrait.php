2c301f45d726413b1b61ee91993668cb5b2a44dd
roboloop <12248453+roboloop@users.noreply.github.com>
2019-11-10 01:58:49 +0000

    Add test helper trait for array population assertion

---
+++ tests/Helpers/AssertArrayTrait.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Tests\Helpers;
+
+trait AssertArrayTrait
+{
+    public function assertArrayPopulation(array $expected, array $actual)
+    {
+        $isSamePopulation = true;
+
+        foreach ($expected as $expectedItem) {
+            $found = false;
+            foreach ($actual as $actualItem) {
+                if ($expectedItem === $actualItem) {
+                    $found = true;
+                    break;
+                }
+            }
+            if (!$found) {
+                $isSamePopulation = false;
+                break;
+            }
+        }
+
+        $this->assertTrue($isSamePopulation, 'Arrays has different populations');
+    }
+
+}
