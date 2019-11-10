110c585b0e0023c938881f026915769df3f6a8cb
roboloop <12248453+roboloop@users.noreply.github.com>
2019-11-10 02:00:36 +0000

    Implement TopicCreator to parse titles and assemble genres, studios and forum

--- src/Application/Creator/TopicCreator.php
+++ src/Application/Creator/TopicCreator.php
@@ -2,12 +2,50 @@
 
 namespace App\Application\Creator;
 
+use App\Domain\Service\ForumService;
+use App\Domain\Service\GenreService;
+use App\Domain\Service\StudioService;
+use App\Domain\Service\TopicService;
 use App\Dto\RawTopicDto;
+use App\Infrastructure\Util\Parser\Title\TitleParserManager;
 
 class TopicCreator
 {
+    private $parserManager;
+    private $genreService;
+    private $studioService;
+    private $forumService;
+    private $topicService;
+
+    public function __construct(
+        TitleParserManager $parserManager,
+        GenreService $genreService,
+        StudioService $studioService,
+        ForumService $forumService,
+        TopicService $topicService
+    ) {
+        $this->parserManager    = $parserManager;
+        $this->genreService     = $genreService;
+        $this->studioService    = $studioService;
+        $this->forumService     = $forumService;
+        $this->topicService     = $topicService;
+    }
+
     public function createFromDto(RawTopicDto $dto)
     {
-        $dto->getForumId();
+        $rawGenres  = $this->parserManager->genres($dto->getRawTitle());
+        $genres     = $this->genreService->getOrMakeOrBoth($rawGenres);
+
+        $rawStudios = $this->parserManager->studios($dto->getRawTitle());
+        $studios    = $this->studioService->getOrMakeOrBoth($rawStudios);
+
+        $forum      = $this->forumService->getOrMake($dto->getForumId(), $dto->getForumTitle());
+
+        // TODO:
+        $images     = [];
+
+        $topic      = $this->topicService->makeNotLoaded($dto->getTrackerId(), $dto->getRawTitle(), $forum, $dto->getSize(), $dto->getTrackerCreatedAt());
+
+        // TODO:
     }
 }
