fd3bf13babe9a76c216b56b6dbe2b0f467efeb0a
roboloop <12248453+roboloop@users.noreply.github.com>
2019-11-14 05:30:29 +0000

    Implement under-spoiler image creation with original URL deduction
    - Deduct and persist original image URL; fallback to null when unsupported
    - Support spoiler-based format; update API to accept spoiler name and inject deduction

--- src/Domain/Service/ImageService.php
+++ src/Domain/Service/ImageService.php
@@ -2,6 +2,8 @@
 
 namespace App\Domain\Service;
 
+use App\Domain\Deduction\NoOriginalUrlDeductionSupportsException;
+use App\Domain\Deduction\OriginalUrlDeductionInterface;
 use App\Domain\Entity\Enum\ImageFormat;
 use App\Domain\Entity\Topic;
 use App\Domain\Factory\ImageFactory;
@@ -11,11 +13,13 @@ class ImageService
 {
     private $imageFactory;
     private $imageRepository;
+    private $urlDeduction;
 
-    public function __construct(ImageFactory $imageFactory, ImageRepositoryInterface $imageRepository)
+    public function __construct(ImageFactory $imageFactory, ImageRepositoryInterface $imageRepository, OriginalUrlDeductionInterface $urlDeduction)
     {
-        $this->imageFactory = $imageFactory;
-        $this->imageRepository = $imageRepository;
+        $this->imageFactory         = $imageFactory;
+        $this->imageRepository      = $imageRepository;
+        $this->urlDeduction         = $urlDeduction;
     }
 
     public function makePosterImage(Topic $topic, string $frontUrl)
@@ -25,8 +29,17 @@ class ImageService
         $this->imageRepository->save($image);
     }
 
-    public function makeUnderSpoilerImage(Topic $topic, string $frontUrl, ?string $reference)
+    public function makeUnderSpoilerImage(Topic $topic, string $frontUrl, ?string $reference, string $spoilerName)
     {
-        // TODO:
+        try {
+            $original = $this->urlDeduction->deduct($frontUrl, ['reference' => $reference]);
+        } catch (NoOriginalUrlDeductionSupportsException $e) {
+            $original = null;
+        }
+
+        $format = ImageFormat::createFromSpoilerName($spoilerName);
+        $image  = $this->imageFactory->make($topic, $format, $frontUrl, $reference, $original);
+
+        $this->imageRepository->save($image);
     }
 }
