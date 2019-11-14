a05eac0660e7c391e5eb3533cd671736f68f39b1
roboloop <12248453+roboloop@users.noreply.github.com>
2019-11-14 05:30:36 +0000

    Attach poster and spoiler images to topics on creation

--- src/Application/Creator/TopicCreator.php
+++ src/Application/Creator/TopicCreator.php
@@ -2,16 +2,17 @@
 
 namespace App\Application\Creator;
 
+use App\Application\Dto\RawImageDto;
 use App\Application\Dto\RawTopicDto;
 use App\Application\Exception\InvalidArgumentWhenCreatingTopicException;
 use App\Domain\Service\ForumService;
 use App\Domain\Service\GenreService;
+use App\Domain\Service\ImageService;
 use App\Domain\Service\StudioService;
 use App\Domain\Service\TopicService;
 use App\Domain\Shared\DateTimeUtilInterface;
 use App\Infrastructure\Util\Parser\Title\TitleParserManager;
 use App\Infrastructure\Util\SizeConverter;
-use Assert\AssertionFailedException;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class TopicCreator
@@ -22,6 +23,7 @@ class TopicCreator
     private $studioService;
     private $forumService;
     private $topicService;
+    private $imageService;
     private $dateTimeUtil;
     private $sizeConverter;
 
@@ -32,6 +34,7 @@ class TopicCreator
         StudioService $studioService,
         ForumService $forumService,
         TopicService $topicService,
+        ImageService $imageService,
         DateTimeUtilInterface $dateTimeUtil,
         SizeConverter $sizeConverter
     ) {
@@ -41,6 +44,7 @@ class TopicCreator
         $this->studioService    = $studioService;
         $this->forumService     = $forumService;
         $this->topicService     = $topicService;
+        $this->imageService     = $imageService;
         $this->dateTimeUtil     = $dateTimeUtil;
         $this->sizeConverter    = $sizeConverter;
     }
@@ -57,12 +61,14 @@ class TopicCreator
         $studios    = $this->studioService->getOrMakeOrBoth($rawStudios);
 
         $forum      = $this->forumService->getOrMake($dto->getForumExId(), $dto->getForumTitle());
-
-        // TODO:
-        $images     = [];
-        $images = $this->image
-
         $topic      = $this->topicService->makeNotLoaded($dto->getExId(), $dto->getRawTitle(), $forum, $dto->getSize(), $dto->getExCreatedAt());
+
+        /** @var \App\Application\Dto\RawImageDto $image */
+        foreach ($dto->getImages() as $image) {
+            $image->getPlace() === RawImageDto::PLACE_ON_PAGE
+                ? $this->imageService->makePosterImage($topic, $image->getFrontUrl())
+                : $this->imageService->makeUnderSpoilerImage($topic, $image->getFrontUrl(), $image->getReference(), $image->getSpoilerName());
+        }
 
         foreach ($genres as $genre) {
             $topic->addGenre($genre);
@@ -72,25 +78,14 @@ class TopicCreator
             $topic->addStudio($studio);
         }
 
-
         return $topic;
     }
 
     private function validateRawTopicDto(RawTopicDto $dto)
     {
-        try {
-            $errors = $this->validator->validate($dto);
+        $errors = $this->validator->validate($dto);
 
-            if ($errors->count()) {
-                throw new InvalidArgumentWhenCreatingTopicException;
-            }
-            // Assertion::notEmpty($dto->getExId(), 'Ex id must be specified');
-            // Assertion::notEmpty($dto->getRawTitle(), 'Raw title must be specified');
-            // Assertion::notEmpty($dto->getForumTitle(), 'Forum title must be specified');
-            // Assertion::notEmpty($dto->getForumExId(), 'Forum ex id must be specified');
-            // Assertion::notEmpty($dto->getSize(), 'Size must be specified');
-            // Assertion::notEmpty($dto->getExCreatedAt(), 'Ex created at timestamp must be specified');
-        } catch (AssertionFailedException $e) {
+        if ($errors->count()) {
             throw new InvalidArgumentWhenCreatingTopicException;
         }
     }
