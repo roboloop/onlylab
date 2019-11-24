396aa7a0aea9785930bc2543146834e16f3498c3
roboloop <12248453+roboloop@users.noreply.github.com>
2019-11-24 18:28:25 +0000

    Add ArrayTopicRepository stub for tests

---
+++ tests/Stubs/Infrastructure/Repository/ArrayTopicRepository.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace OnlyTracker\Tests\Stubs\Infrastructure\Repository;
+
+use OnlyTracker\Domain\Repository\TopicRepositoryInterface;
+
+class ArrayTopicRepository extends ArrayRepository implements TopicRepositoryInterface
+{
+}
