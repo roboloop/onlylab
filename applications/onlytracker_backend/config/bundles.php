d111da31ccd65bbe9a3671166b4c48fcb231ef20
roboloop <12248453+roboloop@users.noreply.github.com>
2019-11-25 04:49:06 +0000

    Integrate Webpack Encore for asset management

--- applications/onlytracker_backend/config/bundles.php
+++ applications/onlytracker_backend/config/bundles.php
@@ -15,4 +15,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Snc\RedisBundle\SncRedisBundle::class => ['all' => true],
     Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
