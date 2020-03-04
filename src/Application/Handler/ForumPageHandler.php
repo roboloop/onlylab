7b598cddaac28a6dc320ba50101186cc0c44dc5f
roboloop <12248453+roboloop@users.noreply.github.com>
2020-03-04 11:18:26 +0000

    Add ForumPageHandler to parse forum pages and create topics

--- src/Application/Handler/ForumPageHandler.php
+++ src/Application/Handler/ForumPageHandler.php
@@ -2,7 +2,27 @@
 
 namespace OnlyTracker\Application\Handler;
 
+use OnlyTracker\Application\CRUD\TopicCreationInterface;
+use OnlyTracker\Application\Parser\ForumHtmlParser;
+use OnlyTracker\Domain\Repository\TopicRepositoryInterface;
+
 class ForumPageHandler
 {
+    private TopicCreationInterface $topicCreator;
+    private ForumHtmlParser $htmlParser;
+    private TopicRepositoryInterface $topicRepository;
 
+    public function __construct(TopicCreationInterface $topicCreator, ForumHtmlParser $htmlParser, TopicRepositoryInterface $topicRepository)
+    {
+        $this->topicCreator     = $topicCreator;
+        $this->htmlParser       = $htmlParser;
+        $this->topicRepository  = $topicRepository;
+    }
+
+    public function handle(string $content)
+    {
+        $rawTopicDtos = $this->htmlParser->parseTopicsHeaders($content);
+
+        array_walk($rawTopicDtos, fn($dto) => $this->topicCreator->createFromDto($dto));
+    }
 }
