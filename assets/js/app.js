51d6f1e9925447a2e7bb7261de5a8bee0494c4e8
roboloop <12248453+roboloop@users.noreply.github.com>
2020-03-13 13:47:37 +0000

    Add client-side routing and AJAX templates; include Bootstrap
    - Render genres and studios by fetching API data and applying Handlebars templates
    - Load Bootstrap, jQuery and Popper for styling and UI behavior

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,16 +7,63 @@
 
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.css');
+require('bootstrap/dist/css/bootstrap.css');
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
+const $ = require('jquery');
+require('popper.js');
+require('bootstrap');
+import VueRouter from 'vue-router'
+import routes from './routes';
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+let router = new VueRouter({
+    mode: 'history',
+    routes,
+});
 
-import Vue from 'vue'
-import App from './App.vue'
+// let routeTo = router.getMatchedComponents(window.location.pathname);
+let routeTo = router.match(window.location.pathname);
 
-new Vue({
-    el: "#app",
-    components: {App}
-})
+// console.log(routeTo);
+switch(routeTo.name) {
+    case 'genres':
+        // const genreTemplate = require('../templates/genres.hbs');
+
+        $.ajax({
+            url: '/api/genres',
+            method: 'GET',
+            dataType: 'JSON',
+        }).then((data, textStatus, jqXHR) => {
+            console.log(data);
+            console.log(textStatus);
+            console.log(jqXHR);
+            const genreTemplate = require('../templates/genres.hbs');
+            $('#App').append(genreTemplate({genres: data}));
+        });
+        break;
+    case 'studios':
+        $.ajax({
+            url: '/api/studios',
+            method: 'GET',
+            dataType: 'JSON',
+        }).then((data, textStatus, jqXHR) => {
+            console.log(data);
+            console.log(textStatus);
+            console.log(jqXHR);
+            const studioTemplate = require('../templates/studios.hbs');
+            $('#App').append(studioTemplate({studios: data}));
+        });
+
+
+        break;
+    default:
+        const defaultTemplate = require('../templates/base.hbs');
+        $('#App').append(defaultTemplate);
+}
+
+// console.log(window.location);
+//
+// console.log(router.getMatchedComponents(window.location.pathname));
+// console.log(router.match('/genres'));
+// console.log(router.match('/studios'));
+// console.log(router.match('/studios/qwe'));
+// console.log(router.match('/studiosinvalud'));
