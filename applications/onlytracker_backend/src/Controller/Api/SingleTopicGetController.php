4499605f0a5d78fea6a3e347311a4bd43a0133f2
roboloop <12248453+roboloop@users.noreply.github.com>
2020-03-16 13:02:24 +0000

    Add images to single topic response and set date format
    - Return images array and serialize dates as d.m.Y H:i

--- applications/onlytracker_backend/src/Controller/Api/SingleTopicGetController.php
+++ applications/onlytracker_backend/src/Controller/Api/SingleTopicGetController.php
@@ -12,6 +12,7 @@ use OnlyTracker\Infrastructure\Request\RequestSenderInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 use Symfony\Contracts\HttpClient\Exception\ExceptionInterface;
 
@@ -50,12 +51,17 @@ class SingleTopicGetController
         }
 
         $context = [
-            AbstractNormalizer::CIRCULAR_REFERENCE_HANDLER => function ($object, $format, $context) {
-                return $object->getId();
-            },
+            AbstractNormalizer::CIRCULAR_REFERENCE_HANDLER => fn($object, $format, $context) => $object->getId(),
+            DateTimeNormalizer::FORMAT_KEY => 'd.m.Y H:i',
         ];
 
         $normalized = $this->normalizer->normalize($topic, null, $context);
+
+        $normalized['images'] = [
+            [ 'original' => '/storage/1.jpeg',],
+            [ 'original' => '/storage/2.jpeg',],
+            [ 'original' => '/storage/3.jpeg',],
+        ];
 
         return new JsonResponse($normalized);
     }
