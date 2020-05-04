68d0eaf301b8bcf1028351f89d00e8d9f1bb98c9
roboloop <12248453+roboloop@users.noreply.github.com>
2020-05-04 20:05:05 +0000

    Remove snc/redis-bundle, related config, and REDIS_URL

--- applications/onlytracker_backend/config/bundles.php
+++ applications/onlytracker_backend/config/bundles.php
@@ -13,7 +13,6 @@ return [
     OnlyTracker\BackEnd\Bundle\ApplicationBundle::class => ['all' => true],
     BM\BackupManagerBundle\BMBackupManagerBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    Snc\RedisBundle\SncRedisBundle::class => ['all' => true],
     Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
