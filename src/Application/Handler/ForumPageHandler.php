f1f945f53f0262ae90a483a69ba43fff4909a3d5
roboloop <12248453+roboloop@users.noreply.github.com>
2020-08-30 20:05:48 +0000

    Run topic creation in transactions and clear EntityManager per item

--- src/Application/Handler/ForumPageHandler.php
+++ src/Application/Handler/ForumPageHandler.php
@@ -2,6 +2,7 @@
 
 namespace OnlyTracker\Application\Handler;
 
+use Doctrine\ORM\EntityManagerInterface;
 use OnlyTracker\Application\CRUD\TopicCreation;
 use OnlyTracker\Application\Parser\ForumHtmlParser;
 use OnlyTracker\Domain\Repository\TopicRepositoryInterface;
@@ -11,20 +12,25 @@ class ForumPageHandler implements ForumPageHandlerInterface
     private TopicCreation $topicCreator;
     private ForumHtmlParser $htmlParser;
     private TopicRepositoryInterface $topicRepository;
+    private EntityManagerInterface $entityManager;
 
-    public function __construct(TopicCreation $topicCreator, ForumHtmlParser $htmlParser, TopicRepositoryInterface $topicRepository)
+    public function __construct(TopicCreation $topicCreator, ForumHtmlParser $htmlParser, TopicRepositoryInterface $topicRepository, EntityManagerInterface $entityManager)
     {
         $this->topicCreator     = $topicCreator;
         $this->htmlParser       = $htmlParser;
         $this->topicRepository  = $topicRepository;
+        $this->entityManager    = $entityManager;
     }
 
     public function handle(string $content)
     {
         $rawTopicDtos = $this->htmlParser->parseTopicsHeaders($content);
 
-        // TODO: BATCH
-
-        array_walk($rawTopicDtos, fn($dto) => $this->topicCreator->createFromDto($dto));
+        foreach ($rawTopicDtos as $rawTopicDto) {
+            $this->entityManager->transactional(fn() => $this->topicCreator->createFromDto($rawTopicDto));
+            $this->entityManager->clear();
+        }
+        
+        // array_walk($rawTopicDtos, fn($dto) => $this->topicCreator->createFromDto($dto));
     }
 }
