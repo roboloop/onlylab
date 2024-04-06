0825502f5ee415064e5ff7a8e874709fd324eec5
roboloop <12248453+roboloop@users.noreply.github.com>
2024-04-06 19:56:55 +0000

    Add tracker page component with filter, banned term highlighting and hotkeys
    - Client-side filtering with highlight; mark banned genres/studios; hotkeys; open links in new tab

--- onlyweb/src/App.vue
+++ onlyweb/src/App.vue
@@ -1,14 +1,17 @@
 <script setup>
 import TopicComponent from './pages/TopicComponent.vue'
 import ForumComponent from './pages/ForumComponent.vue'
+import TrackerComponent from './pages/TrackerComponent.vue'
 
 const loadTopic = window.location.pathname === '/forum/viewtopic.php'
 const loadForum = window.location.pathname === '/forum/viewforum.php'
+const loadTracker = window.location.pathname === '/forum/tracker.php'
 </script>
 
 <template>
   <TopicComponent v-if="loadTopic"></TopicComponent>
   <ForumComponent v-if="loadForum"></ForumComponent>
+  <TrackerComponent v-if="loadTracker"></TrackerComponent>
 </template>
 
 <style scoped></style>
