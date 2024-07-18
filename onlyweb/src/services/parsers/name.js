386f4e1abf82d0ce3d805d8405fb2546a57aa10d
roboloop <12248453+roboloop@users.noreply.github.com>
2024-07-18 01:49:04 +0000

    Prevent empty entries in parsed names

--- onlyweb/src/services/parsers/name.js
+++ onlyweb/src/services/parsers/name.js
@@ -1,5 +1,5 @@
 export function parseName(title) {
   const rawNames = title.split(/[-—(]/)[0]
 
-  return rawNames.split(/,|&|\band\b|\baka\b/i).map((r) => r.trim())
+  return rawNames.split(/,|&|\band\b|\baka\b/i).map((r) => r.trim()).filter(Boolean)
 }
